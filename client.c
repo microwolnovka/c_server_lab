115b722adbbe1809e2d883caecaf949cdc832894
jack <microwolnovka@gmail.com>
2014-03-26 16:11:54 +0300

    added multiply clients

--- client.c
+++ client.c
@@ -10,23 +10,15 @@ void error(char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
+int get_connect(char * server_name, int portno){
+    int sockfd;
 
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    server = gethostbyname(server_name);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
@@ -39,9 +31,21 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    printf("Please enter the message: ");
+    return sockfd;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+
+
+    char buffer[256];
+    sockfd = get_connect("localhost",4444);
+    printf("Please enter the file location: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
+    puts(buffer);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0)
          error("ERROR writing to socket");
--- server.c
+++ server.c
@@ -4,44 +4,60 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <string.h>
 void error(char *msg)
 {
     perror(msg);
     exit(1);
 }
 
+int create_connection(int portno)
+{
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+       error("ERROR opening socket");
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+             error("ERROR on binding");
+    listen(sockfd,5);
+    return sockfd;
+}
+
+int get_client_connect(int socket_d)
+{
+    struct sockaddr_in client;
+    int client_len, cli_d;
+    client_len = sizeof(client);
+    cli_d = accept(socket_d, (struct sockaddr *) &client, &client_len);
+    if(cli_d < 0) error("error on client connecting");
+    return cli_d;
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-    // if (argc < 2) {
-    //     fprintf(stderr,"ERROR, no port provided\n");
-    //     exit(1);
-    // }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = 2234;
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0;
+    int my_socket, n, connection;
+    char buffer[256];
+    my_socket = create_connection(4444);
+    bzero(buffer,256);
+    while(1){
+        connection = get_client_connect(my_socket);
+        n = read(connection,buffer,255);
+        if(strlen(buffer) == 0) return 0;
+        if (n < 0) error("ERROR reading from socket");
+        printf("Here is the message: %s\n",buffer);
+        n = write(connection,"I got your message",18);
+        if (n < 0) error("ERROR writing to socket");
+        close(connection);
+    }
+
+    return 0;
 }
